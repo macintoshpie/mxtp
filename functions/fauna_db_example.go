eb77f2ebca373cbf3dd8844f363e55fa9183e2f5
Ted Summer <ted.summer2@gmail.com>
2020-04-05 16:51:54 -0600

    feat(functions): fauna db example

---
+++ functions/fauna_db_example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	f "github.com/fauna/faunadb-go/faunadb"
+)
+
+func FaunaDbExampleHandler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	secret := os.Getenv("FAUNA_DB_SECRET")
+	if secret == "" {
+		panic("FAUNA_DB_SECRET environment variable missing")
+	}
+
+	endpoint := f.Endpoint("https://db.fauna.com")
+
+	adminClient := f.NewFaunaClient(secret, endpoint)
+	dbName := "learn-fauna-go"
+
+	res, err := adminClient.Query(
+		f.If(
+			f.Exists(f.Database(dbName)),
+			true,
+			f.CreateDatabase(f.Obj{"name": dbName})))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Finished",
+	}, nil
+}
+
+func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
+	lambda.Start(HelloHandler)
+}
--- functions/go.mod
+++ functions/go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/aws/aws-lambda-go v1.16.0
+	github.com/fauna/faunadb-go v2.0.0+incompatible
 	github.com/stretchr/testify v1.5.1
 )
